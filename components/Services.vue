437edb8b5699f4635a2ef316a905655fd0e2a1a6
Jackson Chegenye <jackson.chegenye@betika.com>
2022-12-14 22:22:53 +0300

    updated services

--- components/Services.vue
+++ components/Services.vue
@@ -13,36 +13,43 @@
     <b-row class="mt-md-5 content-row">
       <b-col lg="4" d-flex>
         <b-card no-body class="h-100">
-          <h1>P2P Loans</h1>
-          <p class="my-4">
-            Stop lending informally to your family and friends, next time the
-            say <i>nisave na soft loan</i> tell them to download nisave and
-            request for a soft loan formally. Nisave enables you to set flexible
-            loan terms, track your loan activity and send out reminders when the
-            loan is due, all within the confines of a binding digital contract.
-          </p>
+          <div class="services__content">
+            <h1>P2P Loans</h1>
+            <p class="my-4">
+              Stop lending informally to your family and friends, next time the
+              say <i>nisave na soft loan</i> tell them to download nisave and
+              request for a soft loan formally. Nisave enables you to set
+              flexible loan terms, track your loan activity and send out
+              reminders when the loan is due, all within the confines of a
+              binding digital contract.
+            </p>
+          </div>
           <b-img src="~/assets/imgs/services/cube.svg" fluid alt="cube"></b-img>
         </b-card>
       </b-col>
       <b-col lg="4" d-flex>
         <b-card no-body class="h-100">
-          <h1>Fundraising Cubes</h1>
-          <p class="my-4">
-            Say goodbye to inconvenient and annoying fundraising on WhatsApp
-            groups, with nisave you can create a fundraising cube, invite others
-            and fundraise with convenience and transparency.
-          </p>
+          <div class="services__content">
+            <h1>Fundraising Cubes</h1>
+            <p class="my-4">
+              Say goodbye to inconvenient and annoying fundraising on WhatsApp
+              groups, with nisave you can create a fundraising cube, invite
+              others and fundraise with convenience and transparency.
+            </p>
+          </div>
           <b-img src="~/assets/imgs/services/fund.svg" fluid alt="fund"></b-img>
         </b-card>
       </b-col>
       <b-col lg="4" d-flex>
         <b-card no-body class="h-100">
-          <h1>Chama Cubes</h1>
-          <p class="my-4">
-            Create chama cubes conveniently with your family and friends and
-            start saving for the future, grow and retain wealth together. Nisave
-            chama cubes is the best in the market.
-          </p>
+          <div class="services__content">
+            <h1>Chama Cubes</h1>
+            <p class="my-4">
+              Create chama cubes conveniently with your family and friends and
+              start saving for the future, grow and retain wealth together.
+              Nisave chama cubes is the best in the market.
+            </p>
+          </div>
           <b-img src="~/assets/imgs/services/loan.svg" fluid alt="loan"></b-img>
         </b-card>
       </b-col>
@@ -50,6 +57,7 @@
   </b-row>
 </template>
 <style lang="scss" scoped>
-// .services {
-// }
+.services {
+  justify-content: space-between;
+}
 </style>
